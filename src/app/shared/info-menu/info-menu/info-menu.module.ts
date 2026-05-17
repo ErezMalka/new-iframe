7469b3dfd610b597314f481dd8e696e677bc0dae
ErezMalka <erez@bite.co.il>
2026-05-17 08:10:50 +0200

    feat(info-menu): add Angular module to export InfoMenuComponent

---
+++ src/app/shared/info-menu/info-menu/info-menu.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InfoMenuComponent } from './info-menu.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [InfoMenuComponent],
+  exports: [InfoMenuComponent]
+})
+export class InfoMenuModule {}
