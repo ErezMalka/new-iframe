2ccdd547ef9d78e30ba80fc2c314b05c1d2541df
ErezMalka <erez@bite.co.il>
2026-05-18 12:18:51 +0200

    feat(info-menu): import MatDialogModule for dialog support

--- src/app/shared/info-menu/info-menu/info-menu.module.ts
+++ src/app/shared/info-menu/info-menu/info-menu.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
 import { InfoMenuComponent } from './info-menu.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   declarations: [InfoMenuComponent],
   exports: [InfoMenuComponent]
 })
